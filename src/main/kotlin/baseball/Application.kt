c7d665cfb899ca2a94b70a3421d96bb72c9f6584
xodyd123 <gksrmf9186@naver.com>
2025-12-03 17:35:58 +0900

    feat: style B

--- src/main/kotlin/baseball/Application.kt
+++ src/main/kotlin/baseball/Application.kt
@@ -3,10 +3,11 @@ package baseball
 import baseball.controller.GameController
 import baseball.domain.computer.Computer
 import baseball.domain.number.RandomNumberGenerator
+import baseball.presenter.GameResultPresenter
 import baseball.view.OutputView
 import baseball.view.InPutView
 
 fun main() {
-    val gameController = GameController(OutputView, InPutView , Computer(RandomNumberGenerator))
+    val gameController = GameController(GameResultPresenter(OutputView), InPutView , Computer(RandomNumberGenerator))
     gameController.run()
 }
--- src/main/kotlin/baseball/view/OutputView.kt
+++ src/main/kotlin/baseball/view/OutputView.kt
@@ -10,47 +10,27 @@ object OutputView {
 
     const val NOTHING = "낫싱"
 
+    fun startGamePrompt() = println("숫자 야구 게임을 시작합니다.")
+    fun inputNumberPrompt() = print("숫자를 입력해주세요 : ")
 
-    fun startGamePrompt() {
-        println("숫자 야구 게임을 시작합니다.")
+
+    fun showGameFinished(strikeCount: Int) {
+        println("${strikeCount}스트라이크")
+        println("${strikeCount}개의 숫자를 모두 맞히셨습니다! 게임 종료")
+        println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
     }
 
-    fun inputNumberPrompt(){
-        print("숫자를 입력해주세요 : ")
-    }
+    fun showBallAndStrike(ball: Int, strike: Int) =
+        println("${ball}볼 ${strike}스트라이크")
 
-    private fun formatGameResult(gameResultDto: GameResultDto): List<String> {
-        val ballCount = gameResultDto.ballCount
-        val strikeCount = gameResultDto.strikeCount
+    fun showStrike(strike: Int) =
+        println("${strike}스트라이크")
 
-        if (gameResultDto.isFinished) {
-            return listOf(
-                "${strikeCount}$STRIKE",
-                "${strikeCount}개의 숫자를 모두 맞히셨습니다! 게임 종료",
-                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."
-            )
-        }
+    fun showBall(ball: Int) =
+        println("${ball}볼")
 
-        val line = when {
-            strikeCount > 0 && ballCount > 0 ->
-                "${ballCount}$BALL ${strikeCount}$STRIKE"
-
-            strikeCount > 0 ->
-                "${strikeCount}$STRIKE"
-
-            ballCount > 0 ->
-                "${ballCount}$BALL"
-
-            else ->
-                NOTHING
-
-        }
-        return listOf(line)
-    }
-
-    fun gameResult(gameResultDto: GameResultDto){
-        formatGameResult(gameResultDto).forEach<String>(::println)
-    }
+    fun showNothing() =
+        println("낫싱")
 
 
 }
